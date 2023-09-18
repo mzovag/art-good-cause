8a2d2f0ff0388cc457a07d12242575890a766bbb
Mariia Vaganova <yarachins@gmail.com>
2023-09-18 18:22:01 +0200

    upd

--- src/components/elements/ImageSlider/index.js
+++ src/components/elements/ImageSlider/index.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import cx from 'classnames';
 
-const index = ({children, className}) => {
+const Slide = ({visibility, src, key}) => {
+
   return (
-    <div className="max-w-lg h-72 flex overflow-hidden relative">
-          {/* {CarouselData.map((slide, index) => {
-            return (
-              <img
-                src={slide.image}
-                alt="This is a carousel slide"
-                key={index}
-                className={
-                  index === this.state.currentSlide
-                    ? "block w-full h-auto object-cover"
-                    : "hidden"
-                }
-              />
-            );
-          })} */}
-        </div>
+    <div className={cx("w-full py-4 animate-slide", { block: visibility, hidden: !visibility })} >
+      <img className="w-full border-3" src={src} alt={`slide-`+key}/>
+    </div>
   )
 }
 
-export default index
+const ImageSlider = ({children, images, className}) => {
+  
+  const[slideIndex, setSlideIndex] = useState(0);
+
+  let timer = 7; // sec
+  const _timer = timer;
+
+
+  useEffect(() => {
+    if (images.length) {
+      // this function runs every 1 second
+      setSlideIndex(0);
+
+      setInterval(() => {
+        setSlideIndex(slideIndex + 1);
+
+      }, 7000);
+    }
+  }, [images, slideIndex]);
+
+  return (
+    <div className="w-full max-w-xl overflow-hidden relative">
+      {images.map((item, i) => 
+        <Slide src={item} key={i} visibility={i === slideIndex}/>
+      )}
+    </div>
+  )
+}
+
+export default ImageSlider
